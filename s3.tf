a8464308ced55eb23d726f3b1a6e98d7a58b63ae
David Franco <david.franco@rsinet.com.br>
2020-03-11 19:53:09 -0300

    Added another s3 bucket; added backend remote configuration to terraform cloud

--- s3.tf
+++ s3.tf
@@ -8,3 +8,14 @@ resource "aws_s3_bucket" "david_dev" {
     Environment = "david_dev"
   }
 }
+
+resource "aws_s3_bucket" "david_hml" {
+  provider = aws.us-east-2
+  bucket = "tf-test-david-hml"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "david_hml"
+  }
+}
